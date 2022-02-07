43af1eb07d29d62f627cc5d70b2617b124d932e2
dmkirshon <kirshon.david@gmail.com>
2022-02-07 10:21:28 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import myName from './myName';
+
+
+function component() {
+  const element = document.createElement('div');
+
+  element.textContent = myName('Cody');
+
+  return element;
+}
+
+document.body.appendChild(component());
