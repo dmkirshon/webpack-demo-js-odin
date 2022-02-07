e8755f3e5236dbbda94c6d1a03022d445a7e3c6e
dmkirshon <kirshon.david@gmail.com>
2022-02-07 11:44:33 +0200

    feat: Incorporate output managmenet plugins

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import myName from './myName';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print.js';
 
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.textContent = myName('Cody');
-  element.classList.add('hello');
 
-  // Add the image to our existing div
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.textContent = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
-
+  element.appendChild(btn);
+  
   return element;
 }
 
